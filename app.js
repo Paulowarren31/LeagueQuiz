dce7ba70190c2cb40a88592d5ce79d7b2a5d6606
Paulowarren31 <Paulowarren31@gmail.com>
2015-01-17 05:03:15 -0500

    finished main functionality

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 
 var app = angular.module('main', []);
 app.controller('mainCtrl',['$scope','$http',function($scope,$http){
-$scope.champions=[];
-$scope.started=false; $http.get('https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=c388af0c-681a-431b-a5fb-b21dd04c7c0a').success(function(data){
-    $scope.before=data.data;
-    var result=[];
-    for(var i in $scope.before){
-      result.push([i,$scope.before[i]]);
-    }
-    $scope.after=result;
-    });
+
+  $scope.champions=[];
+  $scope.started=false;
+  $scope.correct=false; $http.get('https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=c388af0c-681a-431b-a5fb-b21dd04c7c0a').success(function(data){
+  $scope.before=data.data;
+  var result=[];
+  for(var i in $scope.before){
+    result.push([i,$scope.before[i]]);
+  }
+  $scope.after=result;
+  });
   $scope.randomize=function(){
     var random=Math.floor((Math.random()*123)+1)
     $scope.currentChampion=$scope.after[random];
@@ -22,6 +24,13 @@ $scope.started=false; $http.get('https://na.api.pvp.net/api/lol/static-data/na/v
 
   };
   $scope.check=function(toCheck){
-    if(toC)
+    if(toCheck==$scope.currentChampion[1].title){
+      $scope.correct=true;
+      window.alert('CORRECT!')
+    }
+    else{
+      $scope.correct=false;
+      window.alert('NO! IT WAS '+$scope.currentChampion[1].title)
+    }
   };
 }]);
