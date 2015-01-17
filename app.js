569ac473db544ff0815ca5264f593919fbcfcc66
Paulowarren31 <Paulowarren31@gmail.com>
2015-01-17 01:16:34 -0500

    second commit

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var app = angular.module('main', []);
-app.controller('mainCtrl',['$scope',function($scope){
-	$scope.tests=[{name:'paulo'},{name:'warren'}];
+app.controller('mainCtrl',['$scope','$http',function($scope,$http){
+  $scope.test="before";
+  $http.get('https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=c388af0c-681a-431b-a5fb-b21dd04c7c0a').success(function(data){
+    $scope.champions=data.champions;
+  })
 }]);
