0717f1419e2782ff954838568490b801435673cf
Paulowarren31 <Paulowarren31@gmail.com>
2015-01-17 01:37:27 -0500

    third commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var app = angular.module('main', []);
 app.controller('mainCtrl',['$scope','$http',function($scope,$http){
   $scope.test="before";
   $http.get('https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=c388af0c-681a-431b-a5fb-b21dd04c7c0a').success(function(data){
-    $scope.champions=data.champions;
+    $scope.champions=data.data;
+
   })
 }]);
