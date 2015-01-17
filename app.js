bdab7816ba27f6b21e331b0d39d0d7691947cc2f
Paulowarren31 <Paulowarren31@gmail.com>
2015-01-17 03:57:16 -0500

    changed a lot of stuff

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
+
 var app = angular.module('main', []);
 app.controller('mainCtrl',['$scope','$http',function($scope,$http){
-  $scope.test="before";
-  $http.get('https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=c388af0c-681a-431b-a5fb-b21dd04c7c0a').success(function(data){
-    $scope.champions=data.data;
+$scope.champions=[];
+$scope.started=false; $http.get('https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=c388af0c-681a-431b-a5fb-b21dd04c7c0a').success(function(data){
+    $scope.before=data.data;
+    var result=[];
+    for(var i in $scope.before){
+      result.push([i,$scope.before[i]]);
+    }
+    $scope.after=result;
+    });
+  $scope.randomize=function(){
+    var random=Math.floor((Math.random()*123)+1)
+    $scope.currentChampion=$scope.after[random];
+  };
 
-  })
+  $scope.start=function(){
+    $scope.started=true;
+    var random2=Math.floor((Math.random()*123)+1)
+    $scope.currentChampion=$scope.after[random2];
+
+  };
+  $scope.check=function(toCheck){
+    if(toC)
+  };
 }]);
