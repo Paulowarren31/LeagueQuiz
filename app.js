492b7f24c904098aa9a63e059d0533d52f660619
Paulowarren31 <Paulowarren31@gmail.com>
2015-01-18 10:42:54 -0500

    this time for sure

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.controller('mainCtrl',['$scope','$http',function($scope,$http){
   $scope.started=false;
   $scope.correct=false;
   $scope.firstSolved=false;
-  $scope.points=0; 
+  $scope.points=0;
   //initially gets all static champion data.
   $http.get('https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=c388af0c-681a-431b-a5fb-b21dd04c7c0a').success(function(data){
   $scope.before=data.data;
@@ -15,7 +15,6 @@ app.controller('mainCtrl',['$scope','$http',function($scope,$http){
   }
   $scope.after=result;
   });
-
 
   $scope.randomize=function(){
     $scope.globalScore=$scope.getGlobalScore();
@@ -43,33 +42,36 @@ app.controller('mainCtrl',['$scope','$http',function($scope,$http){
         window.alert('CORRECT! You get a point.')
         $scope.randomize();
         $scope.answer=""
-        $scope.addToGlobalScore(1);
+
       }
       else{
+        window.alert('CORRECT! You get a point.')
         $scope.getRandomSpell();
         $scope.answerSpell=""
-        $scope.addToGlobalScore(1);
       }
 
-
-      scope.points=$scope.points+1;
+      $scope.addToGlobalScore(1);
+      $scope.points=$scope.points+1;
+      $scope.globalScore=$scope.getGlobalScore();
 
     }
     else{
       $scope.correct=false;
       if(id==2){
         window.alert('NO! IT WAS '+$scope.spellChampName);
+        $scope.getRandomSpell();
+        $scope.answerSpell=""
       }
       else{
         window.alert('NO! IT WAS '+$scope.currentChampion[1].name);
+        $scope.randomize();
+        $scope.answer=""
       }
 
       $scope.points=$scope.points-1;
-      $scope.randomize();
-      $scope.getRandomSpell();
-      $scope.answer=""
-      $scope.answerSpell=""
-      $scope.globalScore=50;
+      $scope.addToGlobalScore(-1);
+      $scope.globalScore=$scope.getGlobalScore();
+
     }
   };
 
@@ -86,6 +88,10 @@ app.controller('mainCtrl',['$scope','$http',function($scope,$http){
       console.log('added '+add+' to globalscore')
     })
   };
+  $scope.rmToGlobalScore=function(sub){
+    $http.put('http://localhost:8080/api/globalscore/rm/'+sub);
+  }
+
   $scope.getRandomSpell=function(){
     $http.get('https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=spells&api_key=c388af0c-681a-431b-a5fb-b21dd04c7c0a').success(function(data){
       $scope.beforeSpell=data.data;
