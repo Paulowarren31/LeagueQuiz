e36b1b1dd19d6dd77e657bbee00b5725fee7b413
paulowarren31 <paulowarren31@gmail.com>
2015-02-22 12:58:19 -0500

    removed unneeded code

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.controller('mainCtrl',function($scope,$http,$modal,pointService){
   $scope.spellsOn=false;
   $scope.started=false;
   $scope.correct=false;
-  $scope.firstSolved=false;
   $scope.points=pointService.getPts();
 
   //initially gets all static champion data.
@@ -35,20 +34,29 @@ app.controller('mainCtrl',function($scope,$http,$modal,pointService){
     $scope.after=result;
   });
 
+  //Switches the title and spell booleans
   $scope.switch=function(){
     $scope.titlesOn=!$scope.titlesOn
     $scope.spellsOn=!$scope.spellsOn
   }
-
+  /*
+  randomizes the spell and
+  title by creating a random
+  Number and getting the champion
+  that has that index.
+  */
   $scope.randomize=function(){
     $scope.globalScore=$scope.getGlobalScore();
     var random=Math.floor((Math.random()*123)+1)
     $scope.currentChampion=$scope.after[random];
     $scope.getRandomSpell();
   };
-
+  /*
+  the function that is called 
+  when the start button is pressed.
+  Initially gets th
+  */
   $scope.start=function(){
-    $scope.getGlobalScore();
     $scope.started=true;
     var random2=Math.floor((Math.random()*123)+1)
     $scope.currentChampion=$scope.after[random2];
@@ -59,7 +67,6 @@ app.controller('mainCtrl',function($scope,$http,$modal,pointService){
   $scope.check=function(toCheck,key,id){
     if(toCheck.toLowerCase()==key.toLowerCase()){
       $scope.correct=true;
-      $scope.firstSolved=true;
       if(id==1){
         window.alert('CORRECT! You get a point.')
         $scope.randomize();
@@ -114,7 +121,11 @@ app.controller('mainCtrl',function($scope,$http,$modal,pointService){
     $http.put('http://10.0.0.24:8081/api/globalscore/'+add).success(function(data){
     })
   };
-
+  /*
+  executes an http GET request 
+  to get the spells of all champions,
+  randomizes a champion and spell.
+  */
   $scope.getRandomSpell=function(){
     $http.get('https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=spells&api_key=c388af0c-681a-431b-a5fb-b21dd04c7c0a').success(function(data){
       $scope.beforeSpell=data.data;
@@ -124,24 +135,20 @@ app.controller('mainCtrl',function($scope,$http,$modal,pointService){
       }
       $scope.afterSpell=resultSpell;
       var random3=Math.floor((Math.random()*123)+1)
+      //each champion has 4 spells
       var random4=Math.floor((Math.random()*4)+1)
       $scope.test=$scope.afterSpell[random3][1].spells[random4].name;
       $scope.spellChampName=$scope.afterSpell[random3][1].name
 
     })
   };
-
+  //opens the modal for leaderboard, changes controller to rankCtrl
   $scope.openLeaderboard=function(){
     var modalInstance = $modal.open({
       templateUrl: 'partials/leaderboard.html',
       controller: 'rankCtrl'
     });
-
-    modalInstance.result.then(function (selectedItem) {
-      $scope.selected=selectedItem;
-    }, function () {
-      
-    });
+    
   };
 
 
